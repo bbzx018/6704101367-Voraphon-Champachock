84d34c98f4ef60f5c2365f50a74799bbd930dc65
Bbzx <bookbig089@gmail.com>
2025-10-08 09:50:49 +0700

    CI/CD

--- quasar.config.js
+++ quasar.config.js
@@ -31,7 +31,7 @@ export default defineConfig((/* ctx */) => {
     ],
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-file#build
-    build: {
+    build: {publicPath: '/id-name-lastname/',
       target: {
         browser: ['es2022', 'firefox115', 'chrome115', 'safari14'],
         node: 'node20',
